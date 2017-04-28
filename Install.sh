e5acff843f8a95ccb8fdd1940d7b23aeed6713dc
HoolyHoo <ezpz99@gmail.com>
2017-04-28 11:30:24 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,6 +3,7 @@
 cd ~
 sudo apt-get install libpng12-dev -y
 sudo apt-get install python-gpiozero -y
+sudo apt-get install python-serial -y
 sudo git clone https://github.com/AndrewFromMelbourne/raspidmx.git
 cd raspidmx
 sudo make
