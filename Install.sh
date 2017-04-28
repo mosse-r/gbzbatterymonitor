4b5c891ae642456df7db879c469a6995f0c4d271
HoolyHoo <ezpz99@gmail.com>
2017-04-28 12:02:01 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,11 +4,7 @@ cd ~
 sudo apt-get install libpng12-dev -y
 sudo apt-get install python-gpiozero -y
 sudo apt-get install python-serial -y
-sudo git clone https://github.com/AndrewFromMelbourne/raspidmx.git
-cd raspidmx
-sudo make
-cd ~
 sudo git clone https://github.com/HoolyHoo/gbzbatterymonitor.git
-cd gbzbatterymonitor
-sudo chmod 755 HHMonitorStart.sh
+sudo chmod 755 /home/pi/gbzbatterymonitor/HHMonitorStart.sh
+sudo chmod 755 /home/pi/gbzbatterymonitor/Pngview
 sudo sed -i '/\"exit 0\"/!s/exit 0/\/home\/pi\/gbzbatterymonitor\/HHMonitorStart.sh \&\nexit 0/g' /etc/rc.local
