ec8445ff3d1ed1924d986d7072e2423110200ced
HoolyHoo <ezpz99@gmail.com>
2017-04-28 10:16:23 -0400

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~
+sudo apt-get install libpng12-dev -y
+sudo apt-get install python-gpiozero -y
+sudo git clone https://github.com/AndrewFromMelbourne/raspidmx.git
+cd raspidmx
+sudo make
+cd ~
+sudo git clone https://github.com/HoolyHoo/gbzbatterymonitor.git
+cd gbzbatterymonitor
+sudo chmod 755 HHMonitorStart.sh
+sudo sed -i '/\"exit 0\"/!s/exit 0/\/home\/pi\/gbzbatterymonitor\/HHMonitorStart.sh \&\nexit 0/g' /etc/rc.local
