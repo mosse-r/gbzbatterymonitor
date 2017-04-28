f4e1adccfbd297f3bfe144d4ce9cbf736e4c3de1
HoolyHoo <ezpz99@gmail.com>
2017-04-28 12:09:19 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,5 +6,5 @@ sudo apt-get install python-gpiozero -y
 sudo apt-get install python-serial -y
 sudo git clone https://github.com/HoolyHoo/gbzbatterymonitor.git
 sudo chmod 755 /home/pi/gbzbatterymonitor/HHMonitorStart.sh
-sudo chmod 755 /home/pi/gbzbatterymonitor/Pngview
+sudo chmod 755 /home/pi/gbzbatterymonitor/Pngview/pngview
 sudo sed -i '/\"exit 0\"/!s/exit 0/\/home\/pi\/gbzbatterymonitor\/HHMonitorStart.sh \&\nexit 0/g' /etc/rc.local
