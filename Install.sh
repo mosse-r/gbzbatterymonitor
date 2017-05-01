acebcb06e27e13adb46bb7f9b178a4c23e11762a
HoolyHoo <ezpz99@gmail.com>
2017-05-01 19:52:08 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,6 +3,7 @@
 cd ~
 sudo apt-get install libpng12-dev -y
 sudo apt-get install python-gpiozero -y
+sudo apt-get install python-pkg-resources python3-pkg-resources -y
 sudo apt-get install python-serial -y
 sudo git clone https://github.com/HoolyHoo/gbzbatterymonitor.git
 sudo chmod 755 /home/pi/gbzbatterymonitor/HHMonitorStart.sh
